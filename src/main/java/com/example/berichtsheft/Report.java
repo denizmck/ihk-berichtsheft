1b50567f423e7ea167690c470a21f19bb2cc5d83
denizmck <dmalik@caseking.de>
2023-01-11 17:14:07 +0100

    Added XML Reader Code very ugly - needs to be reworked and cleaned up Works fine for now + Report Class got a public debug method (debug only)

--- src/main/java/com/example/berichtsheft/Report.java
+++ src/main/java/com/example/berichtsheft/Report.java
@@ -70,4 +70,18 @@ public class Report {
     public Entry getEntryAtIndex(int index){
         return entries.get(index);
     }
+
+    public void debug(){
+        System.out.println("From       : " + startDate);
+        System.out.println("To         : " + endDate);
+        System.out.println("Department : " + department);
+        for(int i=0; i < entries.size(); i++){
+            Entry tempEntry = entries.get(i);
+            System.out.println("\n" + tempEntry.getTitle());
+            for(int n=0; n < tempEntry.items.size(); n++){
+                System.out.println("- " + tempEntry.items.get(n));
+            }
+        }
+        System.out.println("\n-------------------------------------------\n");
+    }
 }
--- src/main/java/com/example/berichtsheft/XMLReader.java
+++ src/main/java/com/example/berichtsheft/XMLReader.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
+
+import javafx.scene.control.Alert;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
@@ -16,29 +18,90 @@ public class XMLReader {
 
         ArrayList<Report> reportList = new ArrayList<>();
 
-
         try {
             DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
             Document document = documentBuilder.parse(file);
             document.getDocumentElement().normalize();
 
-            NodeList nodeList = document.getElementsByTagName("report");
+            NodeList list = document.getElementsByTagName("report");
 
-            Report tempReport = new Report();
-            ArrayList<Entry> tempEntries = new ArrayList<>();
+            for(int i=0; i < list.getLength(); i++){
+                Report tempReport = new Report();
+                Node reportNode = list.item(i);
+                if(reportNode.getNodeType() == Node.ELEMENT_NODE){
+                    Element reportElement = (Element) reportNode;
 
-            for (int i = 0; i < nodeList.getLength(); i++) {
-                Node node = nodeList.item(i);
-                if(node.getNodeType() == Node.ELEMENT_NODE){
+                    // Dates
+                    NodeList dateList = reportElement.getElementsByTagName("date");
+                    for(int n=0; n < dateList.getLength(); n++){
+                        Node dateNode = dateList.item(n);
+                        if(dateNode.getNodeType() == Node.ELEMENT_NODE){
+                            Element dateElement = (Element) dateNode;
+                            tempReport.setStartDate(dateElement.getElementsByTagName("from").item(0).getTextContent());
+                            tempReport.setEndDate(dateElement.getElementsByTagName("to").item(0).getTextContent());
+                        }
+                    }
+
+                    // Department
+                    tempReport.setDepartment(reportElement.getElementsByTagName("department").item(0).getTextContent());
+
+                    // Entries
+                    ArrayList<Entry> tempEntries = new ArrayList<>();
+                    NodeList entryListList = reportElement.getElementsByTagName("entrylist");
+                    for (int n=0; n < entryListList.getLength(); n++){
+                        Node entrylistNode = entryListList.item(n);
+                        if(entrylistNode.getNodeType() == Node.ELEMENT_NODE){
+                            Element entryListElement = (Element) entrylistNode;
+                            Entry tempEntry = new Entry();
+
+                            NodeList entryList = entryListElement.getElementsByTagName("entry");
+                            for (int y=0; y < entryList.getLength(); y++){
+                                Node entryNode = entryList.item(y);
+                                if(entryNode.getNodeType() == Node.ELEMENT_NODE){
+                                    Element entryElement = (Element) entryNode;
+                                    tempEntry.setTitle(entryElement.getAttribute("title"));
+                                    for(int idx=0; idx < entryElement.getElementsByTagName("item").getLength(); idx++){
+                                        tempEntry.addItem(entryElement.getElementsByTagName("item").item(idx).getTextContent());
+                                    }
+                                }
+                            }
+
+                            tempReport.addEntry(tempEntry);
+                        }
+                    }
 
                 }
+                tempReport.debug();
             }
 
-        } catch (Exception e){
 
+        } catch (Exception e){
+            showAlertMessage();
         }
-        return new ArrayList<>();
+
+        return reportList;
+    }
+
+    private static NodeList loopThroughNodes(NodeList nodeList){
+        return new NodeList() {
+            @Override
+            public Node item(int index) {
+                return null;
+            }
+
+            @Override
+            public int getLength() {
+                return 0;
+            }
+        };
+    }
+
+    private static void showAlertMessage(){
+        Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+        errorAlert.setHeaderText("Datei wird nicht unterstützt");
+        errorAlert.setContentText("Die Datei muss XML sein und der richtigen Struktur entsprechen!");
+        errorAlert.showAndWait();
     }
 
 }
