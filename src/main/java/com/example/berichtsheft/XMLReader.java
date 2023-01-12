8c4df8a78c06e2a33fbf39535ddc37a5854389cf
denizmck <dmalik@caseking.de>
2023-01-12 17:42:39 +0100

    Fixed and cleaned up the XMLReader class

--- src/main/java/com/example/berichtsheft/XMLReader.java
+++ src/main/java/com/example/berichtsheft/XMLReader.java
@@ -2,6 +2,7 @@ package com.example.berichtsheft;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.function.Consumer;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
@@ -14,8 +15,13 @@ import org.w3c.dom.Element;
 
 public class XMLReader {
 
+    /***
+     * Opens a documentBuilder instance to read given XML
+     * Following in constructing a list of Reports from the given XML-File
+     * @param file XML File that needs to be in the format for this programm
+     * @return A list of all Reports in a XML file
+     */
     public static ArrayList<Report> getReportsFromXML(File file){
-
         ArrayList<Report> reportList = new ArrayList<>();
 
         try {
@@ -24,57 +30,7 @@ public class XMLReader {
             Document document = documentBuilder.parse(file);
             document.getDocumentElement().normalize();
 
-            NodeList list = document.getElementsByTagName("report");
-
-            for(int i=0; i < list.getLength(); i++){
-                Report tempReport = new Report();
-                Node reportNode = list.item(i);
-                if(reportNode.getNodeType() == Node.ELEMENT_NODE){
-                    Element reportElement = (Element) reportNode;
-
-                    // Dates
-                    NodeList dateList = reportElement.getElementsByTagName("date");
-                    for(int n=0; n < dateList.getLength(); n++){
-                        Node dateNode = dateList.item(n);
-                        if(dateNode.getNodeType() == Node.ELEMENT_NODE){
-                            Element dateElement = (Element) dateNode;
-                            tempReport.setStartDate(dateElement.getElementsByTagName("from").item(0).getTextContent());
-                            tempReport.setEndDate(dateElement.getElementsByTagName("to").item(0).getTextContent());
-                        }
-                    }
-
-                    // Department
-                    tempReport.setDepartment(reportElement.getElementsByTagName("department").item(0).getTextContent());
-
-                    // Entries
-                    ArrayList<Entry> tempEntries = new ArrayList<>();
-                    NodeList entryListList = reportElement.getElementsByTagName("entrylist");
-                    for (int n=0; n < entryListList.getLength(); n++){
-                        Node entrylistNode = entryListList.item(n);
-                        if(entrylistNode.getNodeType() == Node.ELEMENT_NODE){
-                            Element entryListElement = (Element) entrylistNode;
-                            Entry tempEntry = new Entry();
-
-                            NodeList entryList = entryListElement.getElementsByTagName("entry");
-                            for (int y=0; y < entryList.getLength(); y++){
-                                Node entryNode = entryList.item(y);
-                                if(entryNode.getNodeType() == Node.ELEMENT_NODE){
-                                    Element entryElement = (Element) entryNode;
-                                    tempEntry.setTitle(entryElement.getAttribute("title"));
-                                    for(int idx=0; idx < entryElement.getElementsByTagName("item").getLength(); idx++){
-                                        tempEntry.addItem(entryElement.getElementsByTagName("item").item(idx).getTextContent());
-                                    }
-                                }
-                            }
-
-                            tempReport.addEntry(tempEntry);
-                        }
-                    }
-
-                }
-                tempReport.debug();
-            }
-
+            getReports(document, reportList);
 
         } catch (Exception e){
             showAlertMessage();
@@ -83,20 +39,118 @@ public class XMLReader {
         return reportList;
     }
 
-    private static NodeList loopThroughNodes(NodeList nodeList){
-        return new NodeList() {
-            @Override
-            public Node item(int index) {
-                return null;
+    /***
+     * Generic Method to loop through the nodes of an XML-File and calls a callback method
+     * that injects logic handling the Element resulting from the loop
+     * @param nodeList List of Nodes to be looped through
+     * @param callback an Method that will be called on every loop handling the resulting Element
+     */
+    private static void loopThroughNodes(NodeList nodeList, Consumer<Element> callback){
+        for(int i=0; i < nodeList.getLength(); i++){
+            Node reportNode = nodeList.item(i);
+            if(reportNode.getNodeType() == Node.ELEMENT_NODE){
+                Element element = (Element) reportNode;
+                callback.accept(element);
             }
-
-            @Override
-            public int getLength() {
-                return 0;
-            }
-        };
+        }
     }
 
+    /***
+     * First layer of XML-File
+     * Getting all Reports and calling all methods to gather the data of the lower layers
+     * @param document document from documentBuilder containing the XML-File
+     * @param reportList reference to the ArrayList containing the Reports
+     */
+    private static void getReports(Document document, ArrayList<Report> reportList){
+        NodeList list = document.getElementsByTagName("report");
+
+        loopThroughNodes(list, new Consumer<Element>() {
+            @Override
+            public void accept(Element element) {
+                Report tempReport = new Report();
+
+                getDateOfReport(element, tempReport);
+                getDepartmentOfReport(element, tempReport);
+                getEntries(element, tempReport);
+
+                tempReport.debug();
+                reportList.add(tempReport);
+            }
+        });
+    }
+
+    /***
+     * Lower Layer of XML-File
+     * Getting all entries of a Report and attaching it to the given Report-Object
+     * @param reportElement element of the Report-Layer
+     * @param tempReport reference to the Report-Object
+     */
+    private static void getEntries(Element reportElement, Report tempReport){
+        Element entryListElement = (Element) reportElement.getElementsByTagName("entrylist").item(0);
+        NodeList entryList = entryListElement.getElementsByTagName("entry");
+
+        loopThroughNodes(entryList, new Consumer<Element>() {
+            @Override
+            public void accept(Element element) {
+                Entry tempEntry = new Entry();
+                tempEntry.setTitle( element.getAttribute("title") );
+
+                int itemLength = element.getElementsByTagName("item").getLength();
+                for(int i=0; i < itemLength; i++){
+                    tempEntry.addItem(getTextContent(element, "item", i));
+                }
+
+                tempReport.addEntry(tempEntry);
+            }
+        });
+    }
+
+    /***
+     * Lower Layer of XML-File
+     * Getting the from and to Date as a String and attaching it to the given Report-Object
+     * @param reportElement element of the Report-Layer
+     * @param report reference to the Report-Object
+     */
+    private static void getDateOfReport(Element reportElement, Report report){
+        Element dateElement = (Element) reportElement.getElementsByTagName("date").item(0);
+        report.setStartDate( getTextContent(dateElement, "from") );
+        report.setEndDate( getTextContent(dateElement, "to") );
+    }
+
+    /***
+     * Lower Layer of XML-File
+     * Getting the department info and attaching it to the given Report-Object
+     * @param reportElement element of the Report-Layer
+     * @param report reference to the Report-Object
+     */
+    private static void getDepartmentOfReport(Element reportElement, Report report){
+        report.setDepartment( getTextContent(reportElement, "department") );
+    }
+
+    /***
+     * Shorthand for getting the String content of the first Element in an Element list
+     * @param element element list containing the element
+     * @param tag tag of the element
+     * @return Text content of the element
+     */
+    private static String getTextContent(Element element, String tag){
+        return element.getElementsByTagName(tag).item(0).getTextContent();
+    }
+
+    /***
+     * Shorthand for getting the String content of the Element with the given Index
+     * @param element element list containing the element
+     * @param tag tag of the element
+     * @param index index of the element in the list
+     * @return Text content of the element
+     */
+    private static String getTextContent(Element element, String tag, int index){
+        return element.getElementsByTagName(tag).item( index ).getTextContent();
+    }
+
+    /***
+     * JavaFX Alert Box showing up when loading the XML-File fails
+     */
     private static void showAlertMessage(){
         Alert errorAlert = new Alert(Alert.AlertType.ERROR);
         errorAlert.setHeaderText("Datei wird nicht unterstützt");
